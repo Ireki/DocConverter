32d7ab5715bcbae8515c7c4610fc5aaaac56ec69
Irek <irekinded@gmail.com>
2023-04-23 21:08:03 +0300

    fix

--- tests/WebApiTests/Controllers/ConverterTests.cs
+++ tests/WebApiTests/Controllers/ConverterTests.cs
@@ -1,6 +1,7 @@
 ﻿using DocConverter.Domain.Entities;
 using DocConverter.Infrastructure.Services;
 using Microsoft.AspNetCore.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Xunit;
 
 namespace WebApiTests.Controllers
@@ -8,7 +9,7 @@ namespace WebApiTests.Controllers
     public class ConverterTests
     {
         [Fact]
-        public async Task ConvertFileReturnsFile()
+        public async Task ConvertFile()
         {
             using var fileStream = new FileStream(Path.Combine("Files", "payslip.xlsx"), FileMode.Open);
             var convertibleItem = new ConvertibleItem(new FormFile(fileStream, 0, fileStream.Length, "file", fileStream.Name));
